ef4b06a6e487b09d5aebc252bdbeed852231868c
Vitor Poeta <vitorpoetaaa@gmail.com>
2024-09-15 01:47:45 -0300

    Atualizacoes de novos metodos

--- src/VetoresArraysListas/Main.java
+++ src/VetoresArraysListas/Main.java
@@ -3,12 +3,22 @@ package VetoresArraysListas;
 public class Main {
     public static void main(String[] args) {
 
-        Vetor vetor = new Vetor(2);
+        Vetor vetor = new Vetor(5);
 
-            vetor.adicionar("elemento 1: ");
-            vetor.adicionar("elemento 2: ");
-            vetor.adicionar("elemento 3: ");
+            vetor.adicionar("B");
+            vetor.adicionar("C");
+            vetor.adicionar("D");
+            vetor.adicionar("E");
+            vetor.adicionar("F");
 
+        System.out.println(vetor.tamanho());
+        System.out.println(vetor);
+        System.out.println(vetor.busca(2));
+        System.out.println(vetor.busca("B"));
+
+        vetor.adicionar(0,"A");
+        System.out.println(vetor);
+        System.out.println(vetor.tamanho());
 
     }
 }
--- src/VetoresArraysListas/Vetor.java
+++ src/VetoresArraysListas/Vetor.java
@@ -1,5 +1,7 @@
 package VetoresArraysListas;
 
+import java.util.Arrays;
+
 public class Vetor {
 
         private String [] elementos;
@@ -10,15 +12,8 @@ public class Vetor {
            this.tamanho = 0;
         }
 
-    //public void adicionar(String elemento){
-    //   for (int i=0;i<this.elementos.length;i++){
-    //       if(this.elementos[i] == null){
-    //           this.elementos[i] = elemento;
-    //           break;
-    //       }
-    //   }
-
     public boolean adicionar(String elemento){
+            this.aumentarCapacidade();
             if(this.tamanho < this.elementos.length){
                 this.elementos[this.tamanho] = elemento;
                 this.tamanho++;
@@ -28,5 +23,64 @@ public class Vetor {
             }
     }
 
+    public boolean adicionar(int posicao, String elemento){
 
+        if(!(posicao>=0 && posicao<tamanho)){
+            throw new IllegalArgumentException("Posicao invalida");
+        }
+        this.aumentarCapacidade();
+        //mover todos os elementos
+        for(int i=this.tamanho-1;i>=posicao;i--){
+            this.elementos[i+1] = this.elementos[i];
+        }
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+        return true;
+    }
+
+    public int tamanho(){
+            return this.tamanho;
+    }
+
+    private void aumentarCapacidade(){
+            if(this.tamanho == this.elementos.length){
+                String [] elementosNovos = new String[this.elementos.length * 2];
+                for(int i=0;i<this.tamanho;i++){
+                    elementosNovos[i] = this.elementos[i];
+                }
+                this.elementos = elementosNovos;
+            }
+    }
+
+    public String busca(int posicao){
+            if(!(posicao>=0 && posicao<tamanho)){
+                throw new IllegalArgumentException("Posicao invalida");
+            }
+            return this.elementos[posicao];
+    }
+
+    public int busca(String elemento){
+            for(int i=0;i<this.tamanho;i++){
+                if(this.elementos[i].equals(elemento)){
+                    return i;
+                }
+            }
+            return -1;
+    }
+
+    @Override
+    public String toString() {
+            StringBuilder s = new StringBuilder();
+            s.append("[");
+            for(int i=0;i<this.tamanho-1;i++){
+                s.append(this.elementos[i]);
+                s.append(", ");
+            }
+            if(this.tamanho>0){
+                s.append(this.elementos[this.tamanho-1]);
+            }
+            s.append("]");
+        return s.toString();
+    }
 }
